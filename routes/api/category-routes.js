ecf47145411ac24412ad96e437477f2ff6539f7f
Liza Smirnov <krasnienkov@gmail.com>
2023-05-10 23:22:36 -0700

    finishing up bugs

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,12 +3,11 @@ const { Category, Product } = require('../../models/');
 
 // The `/api/categories` endpoint
 
+// find all categories look at the category seeds
 router.get('/', async(req, res) => {
   try {
-
-  // find all categories look at the category seeds
   const categoryData = await Category.findAll({
-    include: [{model: Product}] //has many products so it comes back as an array 
+    include: [{model: Product}] 
   })
   res.status(200).json(categoryData)
   } catch(err){
@@ -16,67 +15,62 @@ router.get('/', async(req, res) => {
   }
 });
 
-router.get('/:id', async (req, res) => { //wait for it to go to the database and come back so use async
-  try { // find one category by its `id` value
-    const getOneCat = await Category.findOne({
-      where: {
-          id: req.params.id //comes from the client side the url 
-      },
-    include: [{model: Product, attributes: ['id', 'product_name', 'price', 'stock', 'category_id']}]
+// find one category by its `id` value
+router.get('/:id', async (req, res) => { 
+  try { 
+    const categoryData= await Category.findByPk(req.params.id, {
+    include: [{model: Product}]
 });
   if (!categoryData){
     res.json(404).json({message: 'Not working no category'});
     return;
   }
-  res.status(200).json(getOneCat)
+  res.status(200).json(categoryData)
 } catch (err) {
   res.status(500).json(err)
 }
 });
-  // be sure to include its associated Products
 
+// create a new category
 router.post('/', async (req, res) => {
   try {
-    const newCatergory = await Category.create({
-      category_name: req.body.category_name
-  }).then(newCategory =>
-    res.status(200).json(newCategory))
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
   } catch (err){
-    res.status(400).json(err);
+    res.status(500).json(err);
   }
-  // create a new category
 });
 
+// update a category by its `id` value
 router.put('/:id', async (req, res, next) => {
   try {
-    const updateCategory = await Category.update(req.body, {
+    const categoryData = await Category.update(req.body, {
       where: {
         id: req.params.id
       }
     });
-    res.status(200).json(updateCategory);
+    res.status(200).json(categoryData);
   } catch (err){
-    res.status(400).json(err);
+    res.status(500).json(err);
   }
-  // update a category by its `id` value
 });
 
+// delete a category by its `id` value
 router.delete('/:id', async (req, res) => {
   try {
-    const deleteCat = await Category.destroy({
+    const categoryData = await Category.destroy({
       where: {
         id: req.params.id
       }
     });
-  if (deletedCategory) {
+  if (categoryData) {
     res.status(200).json({message: 'Category successfully deleted!'})
   } else {
-    res.status(400).json({message: 'Category not deleted'})
+    res.status(500).json({message: 'Category not deleted'})
   }
   } catch (err){
-    res.status(400).json(err)
+    res.status(500).json(err)
   }
-  // delete a category by its `id` value
 });
 
 module.exports = router;
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -16,21 +16,16 @@ router.get('/', async (req, res) => {
   }
 }),
 
-  // be sure to include its associated Category and Tag data
-  
-  // get one product
+// find a single product by its `id`
   router.get('/:id', async (req, res) => {
-    // find a single product by its `id`
-    // be sure to include its associated Category and Tag data
-    try {
-      const productData = await Product.findByPk(req.params.id,{
+  try {
+    const productData = await Product.findByPk(req.params.id,{
         include: [{model:Category}, {model: ProductTag}],
-      });
+    });
     
-      if (!productData) {
-        res.status(400).json({message: 'No Product found with that id!'})
-        return;
-      } 
+    if (!productData) {
+    res.status(400).json({message: 'No Product found with that id!'})
+    } 
       res.status(200).json(readerData);
     } catch (err) {
       res.status(400).json(err);
